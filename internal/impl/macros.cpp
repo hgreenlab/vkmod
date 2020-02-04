8d81705b14e377cac91afb4af224bb3848dc211f
Holden Green <awesomeholdengreen@gmail.com>
2020-02-03 17:02:58 -0800

    some config updates

---
+++ internal/impl/macros.cpp
@@ -0,0 +1,25 @@
+#include "../macros.hpp"
+
+namespace vkmod
+{
+	namespace layer
+	{
+		namespace google
+		{
+			std::string unique_objects = "VK_LAYER_GOOGLE_unique_objects";
+		}
+		namespace lunarg
+		{
+			std::string api_dump = "VK_LAYER_LUNARG_device_limits";
+			std::string core_validation = "VK_LAYER_LUNARG_image";
+			std::string image = "VK_LAYER_LUNARG_image";
+			std::string object_tracker = "VK_LAYER_LUNARG_object_tracker";
+			std::string parameter_validation = "VK_LAYER_LUNARG_parameter_validation";
+			std::string swapchain = "VK_LAYER_LUNARG_swapchain";
+			std::string threading = "VK_LAYER_GOOGLE_threading";
+			std::string standard_validation = "VK_LAYER_LUNARG_standard_validation";
+			std::string device_limits = "VK_LAYER_LUNARG_device_limits";
+		}
+
+	}
+}
---
+++ internal/macros.hpp
@@ -0,0 +1,25 @@
+#include <string>
+
+namespace vkmod
+{
+	namespace layer
+	{
+		namespace google
+		{
+			std::string unique_objects;
+		}
+		namespace lunarg
+		{
+			std::string api_dump;
+			std::string core_validation;
+			std::string image;
+			std::string object_tracker;
+			std::string parameter_validation;
+			std::string swapchain;
+			std::string threading;
+			std::string standard_validation;
+			std::string device_limits;
+		}
+
+	}
+}
